bb014bfbd099b7d1c50ef76ef82026985dfb4667
ZIFFFF <939967989@qq,com>
2025-02-13 21:59:13 +0800

    同步

--- jeecgboot-vue3/src/views/dashboard/workbench/components/WorkbenchHeader.vue
+++ jeecgboot-vue3/src/views/dashboard/workbench/components/WorkbenchHeader.vue
@@ -2,7 +2,7 @@
   <div class="lg:flex">
     <Avatar :src="userinfo.avatar || headerImg" :size="72" class="!mx-auto !block" />
     <div class="md:ml-6 flex flex-col justify-center md:mt-0 mt-2">
-      <h1 class="md:text-lg text-md">早安, {{ userinfo.realname }}, 开始您一天的工作吧！</h1>
+      <h1 class="md:text-lg text-md">{{ greeting }}, {{ userinfo.realname }}, 开始您一天的工作吧！</h1>
       <span class="text-secondary"> 今日晴，20℃ - 32℃！ </span>
     </div>
     <div class="flex flex-1 justify-end md:mt-0 mt-4">
@@ -30,4 +30,18 @@
 
   const userStore = useUserStore();
   const userinfo = computed(() => userStore.getUserInfo);
+
+  // 获取当前时间并生成问候语
+const greeting = computed(() => {
+  const hour = new Date().getHours();
+  if (hour >= 5 && hour < 12) {
+    return '早安';
+  } else if (hour >= 12 && hour < 14) {
+    return '午安';
+  } else if (hour >= 14 && hour < 18) {
+    return '下午好';
+  } else {
+    return '晚上好';
+  }
+});
 </script>
--- jeecgboot-vue3/src/views/dashboard/workbench/index.vue
+++ jeecgboot-vue3/src/views/dashboard/workbench/index.vue
@@ -2,32 +2,24 @@
   <PageWrapper>
     <template #headerContent> <WorkbenchHeader /> </template>
     <div class="lg:flex">
-      <div class="lg:w-7/10 w-full !mr-4 enter-y">
-        <ProjectCard :loading="loading" class="enter-y" />
+      
+      <div class="lg:w-8/10 w-full !mr-4 enter-y">
+        <FullCalendar :loading="loading" class="enter-y" />
+      </div>  
+      <div class="lg:w-2/10 w-full enter-y">
+        <QuickNav :loading="loading" />
         <DynamicInfo :loading="loading" class="!my-4 enter-y" />
-      </div>
-      <div class="lg:w-3/10 w-full enter-y">
-        <QuickNav :loading="loading" class="enter-y" />
-
-        <Card class="!my-4 enter-y" :loading="loading">
-          <img class="xl:h-50 h-30 mx-auto" src="../../../assets/svg/illustration.svg" />
-        </Card>
-
-        <SaleRadar :loading="loading" class="enter-y" />
-      </div>
+      </div> 
     </div>
   </PageWrapper>
 </template>
 <script lang="ts" setup>
   import { ref } from 'vue';
-  import { Card } from 'ant-design-vue';
   import { PageWrapper } from '/@/components/Page';
   import WorkbenchHeader from './components/WorkbenchHeader.vue';
-  import ProjectCard from './components/ProjectCard.vue';
+  import FullCalendar from './components/FullCalendar.vue';
   import QuickNav from './components/QuickNav.vue';
   import DynamicInfo from './components/DynamicInfo.vue';
-  import SaleRadar from './components/SaleRadar.vue';
-
   const loading = ref(true);
 
   setTimeout(() => {
